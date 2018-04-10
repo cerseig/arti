50e6de3a8df2be8340144a762bec978ff2a38640
Circé <circegrand@gmail.com>
2018-04-10 15:35:28 +0200

    Add voices + add animation on arti robot on click event

--- app/js/modules/_animationArti.js
+++ app/js/modules/_animationArti.js
@@ -2,18 +2,20 @@ export {animationArti}
 
 function animationArti() {
 
+var timeout;
+document.onmousemove = function(){
+  clearTimeout(timeout);
+  timeout = setTimeout(function(){
+    $('.introduction__animation').attr('src', 'images/arti/arti_anim_1.gif');
+  }, 10000);
+}
+document.body.onclick = function() {
+  $('.introduction__animation').attr('src', 'images/arti/arti_anim_5.gif');
+}
+
 var counter = 0;
 $('.introduction__animation').bind("click",function(){
     counter++;
-    // if(counter == 1) {
-    //   console.log('1 click');
-    //     $('.introduction__animation').attr('src', 'images/arti/arti_anim_6.gif');
-    //     setTimeout(function(){
-    //         $('.introduction__animation').attr('src', 'images/arti/arti_anim_5.gif');
-    //       }, 1000);
-    //   counter = 0;
-    // }
-
     setTimeout(function(){
       if(counter == 1) {
         console.log('1 click');
@@ -28,7 +30,7 @@ $('.introduction__animation').bind("click",function(){
         $('.introduction__animation').attr('src', 'images/arti/arti_anim_4.gif');
         setTimeout(function(){
             $('.introduction__animation').attr('src', 'images/arti/arti_anim_5.gif');
-          }, 5000);
+          }, 4000);
         counter = 0;
       }
       else if(counter == 5) {
@@ -36,17 +38,10 @@ $('.introduction__animation').bind("click",function(){
         $('.introduction__animation').attr('src', 'images/arti/arti_anim_3.gif');
         setTimeout(function(){
             $('.introduction__animation').attr('src', 'images/arti/arti_anim_5.gif');
-          }, 5000);
+          }, 4000);
         counter = 0;
       }
     }, 1000);
 })
-
-  // $('.introduction__animation').on('click', function() {
-  //   $(this).attr('src', 'images/arti/arti_anim_6.gif');
-  //   setTimeout(function(){
-  //     $('.introduction__animation').attr('src', 'images/arti/arti_anim_5.gif');
-  //   }, 1000);
-  // })
 
 }
